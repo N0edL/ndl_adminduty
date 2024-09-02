c2fdfafec5fb8e7e1cc5569b35fc0f55af2d81dd
NoedL <88205753+N0edL@users.noreply.github.com>
2024-09-02 19:49:49 +0200

    Pused the 1.0.2 update
    Made everything opensource (uploaded the v1.0.1 accidentally encoded).
    I made some minor changes to both the client and server code

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,15 +4,14 @@ lua54           "yes"
 use_fxv2_oal    "yes"
 
 author          "NoedL"
-version         "1.0.1"
+version         "1.0.2"
 license         "MIT"
 repository      "https://github.com/N0edL/ndl_adminduty"
 
-
 shared_scripts {
-    '@es_extended/imports.lua',
-    'settings.lua'
+    '@es_extended/imports.lua'
 }
+
 client_script   "client.lua"
 server_script   "server.lua"
 ui_page         "html/index.html"
--- html/script.js
+++ html/script.js
@@ -1,14 +1,81 @@
-// Copyright (c) Facebook, Inc. and its affiliates.
-// 
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
+$(function () {
+    function display(bool) {
+        if (bool) {
+            $("#container").fadeIn(300);
+        } else {
+            $("#container").fadeOut(0);
+        }
+    }
+    display(false);
+    window.addEventListener('message', function (event) {
+        var item = event.data;
+        if (item.type === "off") {
+            if (item.status == true) {
+                display(true);
+            } else {
+                display(false);
+            }
+        }
 
-$(function(){function display(bool){if(bool){$("#container").fadeIn(300)}else{$("#container").fadeOut(0)}}
-display(!1);window.addEventListener('message',function(event){var item=event.data;if(item.type==="ui"){if(item.status==!0){display(!0)}else{display(!1)}}
-if(item.type==="Info"){$("#name").html(item.name+"!");if(item.dienst==!0){$("#dutybutton").html("Go off duty");$("#dutybutton").css("background-color","#ca0d26");$("#dutybutton:hover").css("background-color","#c20d25")}else $("#dutybutton").html("Go on duty");if(item.logs==!0){$("#extrabutton").html("Extras <br>On");$("#extrabutton").css("background-color","#0d4535");$("#extrabutton:hover").css("background-color","#c20d25")}else $("#extrabutton").html("Extras <br>Off")}
-if(item.type==='updatePlayerInfo'){updatePlayerList(event.data.playerList)}})
-document.addEventListener('keyup',function(event){if(event.code==='Escape'){$.post('https://ndl_adminduty/exit',JSON.stringify({}));return}});$("#dutybutton").click(function(){$.post('https://ndl_adminduty/dutybutton',JSON.stringify({}));return})
-$("#extrabutton").click(function(){$.post('https://ndl_adminduty/extrabutton',JSON.stringify({}));return})})
-function updatePlayerList(playerList){const playerListElement=document.getElementById('players');playerListElement.innerHTML='';if(playerList&&Array.isArray(playerList)){playerList.forEach(playerInfo=>{const playerDiv=document.createElement('div');playerDiv.id='player'
-const playerName=document.createElement('p');playerName.innerText=playerInfo.name;if(playerInfo.self){playerName.style.color='#1d735b'}
-playerDiv.appendChild(playerName);playerListElement.appendChild(playerDiv)})}}
+        if (item.type === "Info") {
+            $("#name").html(item.name + "!");
+            if (item.dienst == true) {
+                $("#dutybutton").html("Go off duty");
+                $("#dutybutton").css("background-color", "#ca0d26");
+                $("#dutybutton:hover").css("background-color", "#c20d25");
+            } else
+                $("#dutybutton").html("Go on duty");
+            
+            if (item.logs == true) {
+                $("#extrabutton").html("Extras <br>On");
+                $("#extrabutton").css("background-color", "#0d4535");
+                $("#extrabutton:hover").css("background-color", "#c20d25");
+            }
+            else
+                $("#extrabutton").html("Extras <br>Off");
+
+            
+        }
+
+        if (item.type === 'updatePlayerInfo') {
+            updateadminList(event.data.adminList);
+        }
+    })
+    document.onkeyup = function (data) {
+        if (data.which == 27) {
+            $.post('https://ndl_adminui/exit', JSON.stringify({}));
+            return;
+        }
+    };
+
+    $("#dutybutton").click(function () {
+        $.post('https://ndl_adminui/dutybutton', JSON.stringify({}));
+        return
+    })
+
+    $("#extrabutton").click(function () {
+        $.post('https://ndl_adminui/extrabutton', JSON.stringify({}));
+        return
+    })
+})
+function updateadminList(adminList) {
+    const adminListElement = document.getElementById('players');
+    adminListElement.innerHTML = '';
+
+    if (adminList && Array.isArray(adminList)) {
+        adminList.forEach(playerInfo => {
+            // console.log(playerInfo);
+            const playerDiv = document.createElement('div');
+            playerDiv.id = 'player'
+            const playerName = document.createElement('p');
+
+            playerName.innerText = playerInfo.name;
+
+            if (playerInfo.self) {
+                playerName.style.color = '#1d735b';
+            }
+            playerDiv.appendChild(playerName);
+            adminListElement.appendChild(playerDiv);
+        });
+    }
+}
